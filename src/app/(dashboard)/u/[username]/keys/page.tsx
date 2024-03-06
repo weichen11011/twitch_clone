323c5006c4227d285cd85e702beb723892b71900
weichen11011 <yan10110614@gmail.com>
2024-03-06 18:00:07 +0800

    0306

---
+++ src/app/(dashboard)/u/[username]/keys/page.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button"
+import { UrlCard } from "./_components/url-card"
+import { getSelf } from "@/lib/auth-service"
+import { getStreamByUserId } from "@/lib/stream-service"
+import { KeyCard } from "./_components/key-card"
+import { ConnecttModal } from "./_components/connect-modal"
+
+const KeysPage = async() => {
+    const self = await getSelf()
+    const stream = await getStreamByUserId(self.id)
+
+
+    if(!stream) {
+        throw new Error("Streams not Found")
+    }
+    return (
+        <div className="p-6">
+            <div className="flex items-center justify-between mb-4">
+                <h1 className="text-2xl font-bold">
+                    Keys & URLs
+                </h1>
+                <ConnecttModal />
+                <Button variant="primary">
+                    Generate
+                </Button>
+            </div>
+            <div className="space-y-4">
+                <UrlCard value={stream.serverUrl} />
+                <KeyCard value={stream.streamKey} />
+
+            </div>
+        </div>
+    )
+}
+
+export default KeysPage
